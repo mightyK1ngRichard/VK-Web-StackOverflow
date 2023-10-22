31643fd77d667a418d6fd05cf7b70ea80b106411
Dmitriy Permyakov <permyakov.d5@wb.ru>
2023-10-22 03:29:18 +0300

    Finish task01
    minor

--- askme/views.py
+++ askme/views.py
@@ -2,43 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
+class Question:
+    def __init__(self, title, content, rating, author_img):
+        self.title = title
+        self.content = content
+        self.rating = rating
+        self.author_img = author_img
+
+
+question_item = Question(
+    'Sample Title',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
+    777,
+    'https://sun9-74.userapi.com/impg/IkdqPKOIfRr8TF5XrDpx0FApStrqI3N00iDHmw/McWp1do3_sU.jpg?size=1080x1080&quality=95&sign=050a9d0bbcbb22084f2f41f5574b7c71&type=album'
+)
+
+
 def index(request):
-    class Question:
-        def __init__(self, title, content, author_img):
-            self.title = title
-            self.content = content
-            self.author_img = author_img
-
-    questions = [
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-        Question('Sample Title',
-                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget tortor at odio tempus scelerisque.',
-                 'https://sun9-76.userapi.com/impg/9UT7SzAC_gHL2GBES69P-hXudzqKmS8_f0oPXA/AN2fmitt7Bs.jpg?size=1080x1920&quality=95&sign=812fa659cab85e0be2fc74516dc27ae7&type=album'),
-    ]
-
+    questions = [question_item for _ in range(3)]
     return render(request, 'index.html', {'questions': questions})
 
 
@@ -54,19 +35,18 @@ def login(request):
     return render(request, 'login.html')
 
 
+def tag_list(request, tag):
+    questions = [question_item for _ in range(1)]
+    return render(request, 'index.html', {'questions': questions})
+
+
 def signup(request):
     return render(request, 'signup.html')
 
 
 def question(request, question_id):
-    answers = [
-        'Просто какой-то текст',
-        'Просто какой-то текст',
-        'Просто какой-то текст',
-        'Просто какой-то текст',
-        'Просто какой-то текст',
-    ]
-    return render(request, 'question.html', {'answers': answers})
+    answers = ['Просто какой-то текст' for _ in range(3)]
+    return render(request, 'question.html', {'question': question_item, 'answers': answers})
 
 
 def page_not_found(request, exception):
